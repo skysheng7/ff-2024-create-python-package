1b9988452ee6c19aa73391a4ecef00e413810c2a
Sky <82303914+skysheng7@users.noreply.github.com>
2024-10-30 08:57:48 -0700

    updated a function

---
+++ src/my_python_package/add_numbers.py
@@ -0,0 +1,19 @@
+"""A module that adds numbers together"""
+
+
+def add_numbers(num1, num2):
+    """A function that adds two numbers together
+    
+    Parameters
+    ----------
+    num1 : int
+        The first number to add
+
+    num2 : int
+        The second number to add
+    
+    Return
+    ------
+        The sum of two numbers
+
+    """
